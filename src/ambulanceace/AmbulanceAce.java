d919767b77484087051aed84bf1a5aeba536b01f
ridsa00 <ridsashabbir123@gmail.com>
2021-12-05 16:05:59 +0500

    3rd semester
    final term project

---
+++ src/ambulanceace/AmbulanceAce.java
@@ -0,0 +1,17 @@
+package ambulanceace;
+
+/**
+ *
+ * @author DELL
+ */
+public class AmbulanceAce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
